f762a3bef985c31ab27beba21f8f8492bfe79638
HiMyNameIsSunny <thaigrrs@gmail.com>
2021-04-19 23:26:54 +0300

    Dependencies, linter

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "indent": ["error", 4],
+        "no-console": "off"
+    }
+};
