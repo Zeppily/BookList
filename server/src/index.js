ef88c634b38ead89ca0139af9cc851d9126bafb0
HiMyNameIsSunny <thaigrrs@gmail.com>
2021-04-22 21:26:59 +0300

    Allowed CORS to Server

--- server/src/index.js
+++ server/src/index.js
@@ -5,14 +5,22 @@ import bookRoutes from './routes/bookRoutes';
 import knexConfig from './knexfile';
 
 const knex = Knex(knexConfig.development);
+const cors = require('cors');
 
 Model.knex(knex);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/books', bookRoutes);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use((_, res) => {
     res.status(404).json({ status: 404, message: 'Invalid endpoint or method' });
